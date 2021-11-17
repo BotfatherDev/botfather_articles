a10fb85eada855fb71b0bb7ee54ce658ae49c4ca
Gleb Garanin <glebgar567@gmail.com>
2021-11-17 19:03:38 +0200

    bugfix

--- bot.py
+++ bot.py
@@ -8,14 +8,18 @@ from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from tgbot.config import load_config
 from tgbot.filters.admin import AdminFilter
 from tgbot.handlers.admin import register_admin
-from tgbot.handlers.echo import register_echo
 from tgbot.handlers.photos import register_photo_handlers
 from tgbot.handlers.user import register_user
+from tgbot.integrations.telegraph import TelegraphService, FileUploader
 from tgbot.middlewares.db import DbMiddleware
 from tgbot.middlewares.integration import IntegrationMiddleware
-from tgbot.integrations.telegraph import TelegraphService
 
 logger = logging.getLogger(__name__)
+
+
+async def on_shutdown(dp: Dispatcher):
+    file_uploader: FileUploader = dp.bot["file_uploader"]
+    await file_uploader.close()
 
 
 def register_all_middlewares(dp):
@@ -60,6 +64,7 @@ async def main():
         await dp.storage.close()
         await dp.storage.wait_closed()
         await bot.session.close()
+        await on_shutdown(dp)
 
 
 if __name__ == '__main__':
